6494e820d9e1a036d8b4077dc9f3b60a6e1bd311
Shailesh Shenoy <shenoy.shailesh97@gmail.com>
2019-04-13 23:15:57 +0530

    Implemented the basic Percolation class API

--- src/Percolation.java
+++ src/Percolation.java
@@ -7,15 +7,23 @@ public class Percolation
 	private int gridSize;
 	private int[] openSites;
 	private int numberOfSites;
+	private int numberOfOpenSites;
+	private int topVirtualSite;
+	private int bottomVirtualSite;
 	
 	public Percolation(int n)
 	{
 		if (n <= 0)
 			throw new IllegalArgumentException("n must be greater than 0");
-		this.gridSize = n + 1;
-		this.numberOfSites = this.gridSize * this.gridSize;
-		uf = new WeightedQuickUnionUF(this.numberOfSites);
-		openSites = new int[this.numberOfSites];
+		gridSize = n + 1;
+		numberOfSites = gridSize * gridSize;
+		uf = new WeightedQuickUnionUF(numberOfSites);
+		openSites = new int[numberOfSites]; // Initialized to default value 0: closed
+		numberOfOpenSites = 0;
+		topVirtualSite = 0;
+		bottomVirtualSite = (gridSize - 1) * gridSize;
+		openSites[topVirtualSite] = 1;
+		openSites[bottomVirtualSite] = 1;
 	}
 	
 	public static void main(String[] args)
@@ -23,18 +31,127 @@ public class Percolation
 		// TODO Auto-generated method stub
 		
 		System.out.println("");
-		Percolation p = new Percolation(5);
-		System.out.println("Number of components: " + p.uf.count());
-		p.uf.union(1, 23);
-		p.uf.union(7, 23);
-		p.uf.union(5, 6);
-		p.uf.union(5, 8);
-		p.uf.union(6, 9);
-		p.uf.union(7, 6);
-		System.out.println("Number of components: " + p.uf.count());
-		System.out.println(p.uf.find(23));
-		System.out.println(p.uf.find(6));
-		System.out.println(p.uf.find(7));
+		int n = 5;
+		Percolation p = new Percolation(n);
+		int row = 1, col = 1;
+		p.open(row, col);
+		row = 5;
+		col = 5;
+		p.open(row, col);
 	}
 	
+	private boolean validateIndices(int row, int col)
+	{
+		if ((row >= 1 && row <= gridSize - 1) && (col >= 1 && col <= gridSize - 1))
+			return true;
+		else
+			// throw new IllegalArgumentException("The values of row and column must be
+			// between 1
+			// and " + (this.gridSize - 1));
+			return false;
+	}
+	
+	private int calculateIndex(int row, int col)
+	{
+		// Validate Indices
+		if (!this.validateIndices(row, col))
+			throw new IllegalArgumentException("The values of row and column must be between 1 and " + (gridSize - 1));
+		int index = row * this.gridSize + col;
+		return index;
+	}
+	
+	public void open(int row, int col)
+	{
+		// Validate Indices
+		if (!this.validateIndices(row, col))
+			throw new IllegalArgumentException("The values of row and column must be between 1 and " + (gridSize - 1));
+		
+		// Calculate Index
+		int site = calculateIndex(row, col);
+		
+		// Add to open sites
+		openSites[site] = 1;
+		numberOfOpenSites += 1;
+		
+		// if in top row, connect to top virtualSite
+		if (row == 1)
+		{
+			uf.union(topVirtualSite, site);
+		}
+		// if in bottom row, connect to bottom virtualSite
+		if (row == gridSize - 1)
+		{
+			uf.union(bottomVirtualSite, site);
+		}
+		// Check adjacent sites and connect if open
+		// Top neighbor
+		int adjacentSite;
+		if (this.validateIndices(row - 1, col))
+		{
+			if (this.isOpen(row - 1, col))
+			{
+				adjacentSite = this.calculateIndex(row - 1, col);
+				uf.union(site, adjacentSite);
+			}
+		}
+		// Left neighbor
+		if (this.validateIndices(row, col - 1))
+		{
+			if (this.isOpen(row, col - 1))
+			{
+				adjacentSite = this.calculateIndex(row, col - 1);
+				uf.union(site, adjacentSite);
+			}
+		}
+		// Bottom neighbor
+		if (this.validateIndices(row + 1, col))
+		{
+			if (this.isOpen(row + 1, col))
+			{
+				adjacentSite = this.calculateIndex(row + 1, col);
+				uf.union(site, adjacentSite);
+			}
+		}
+		// Right neighbor
+		if (this.validateIndices(row, col + 1))
+		{
+			if (this.isOpen(row, col + 1))
+			{
+				adjacentSite = this.calculateIndex(row, col + 1);
+				uf.union(site, adjacentSite);
+			}
+		}
+		
+	}
+	
+	public boolean isOpen(int row, int col)
+	{
+		if (openSites[calculateIndex(row, col)] == 1)
+			return true;
+		else
+			return false;
+		
+	}
+	
+	public int numberOfOpenSites()
+	{
+		return numberOfOpenSites;
+	}
+	
+	public boolean isFull(int row, int col)
+	{
+		int site = calculateIndex(row, col);
+		if (uf.connected(topVirtualSite, site))
+			return true;
+		else
+			return false;
+	}
+	
+	public boolean percolates()
+	{
+		if (uf.connected(topVirtualSite, bottomVirtualSite))
+			return true;
+		else
+			return false;
+	}
 }
